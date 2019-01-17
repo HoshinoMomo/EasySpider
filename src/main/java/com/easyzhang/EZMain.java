d80e6c24d75b0d0fb1d3ee52d6f1535613ced50f
zhangyi02 <zhangyi02@daojia-inc.com>
2019-01-17 18:56:05 +0800

    修改jar

--- src/main/java/com/easyzhang/EZMain.java
+++ src/main/java/com/easyzhang/EZMain.java
@@ -4,24 +4,30 @@ import com.easyzhang.operate.EzConnection;
 import com.easyzhang.operate.EzDownload;
 import com.easyzhang.util.EzDownloadQueue;
 import com.easyzhang.util.EzWaitQueue;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 
 /**
  * Created by EasyZhang on 2017-12-26.
  */
 public class EZMain {
-      public static void main(String[] args){
-        //初始化下载队列队列
-        EzDownloadQueue.createEzDownloadQueue();
-        //初始化等待扫描URL的队列
-         EzWaitQueue.createEzWaitQueue();
-        //启动下载线程
-        new Thread(new EzDownload()).start();
+      public static void main(String[] args) throws Exception{
 
-        String baseURL = "http://www.baidu.com";
-        EzWaitQueue.getInstance().push(baseURL);
 
-        new Thread(new EzConnection(baseURL)).start();
-
+        String baseURL = "http://tech.163.com/19/0117/01/E5ME6STK00097U81.html";
+        Document doc = Jsoup.connect(baseURL).get();
+        String title = doc.select("meta[property=og:title]").attr("content");
+        String tag = doc.select("meta[property=article:tag]").attr("content");
+        String href = doc.select("a[id=ne_article_source]").attr("href");
+        String source = doc.select("a[id=ne_article_source]").text();
+        Elements elements = doc.getElementById("endText").select("p");
+        String content = "";
+        for(Element element : elements){
+            content += element.toString();
+        }
+        System.out.println(content);
     }
 }
